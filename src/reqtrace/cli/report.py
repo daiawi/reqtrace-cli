9c222f62b19e1be143ab3b4bb0ea3a963228e714
daniel <daiawi972@gmail.com>
2026-08-17 15:23:11 -0400

    Collect all requirements and test traces in project

--- src/reqtrace/cli/report.py
+++ src/reqtrace/cli/report.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import click
 
 from ..core.discover import Package, find_packages
-from .parse import _parse_requirements
+from .parse import _collect_pytest, _parse_requirements
 
 
 @click.command()
@@ -20,9 +20,19 @@ def report(dir):
 
 	packages = [pkg for pkg in project.packages if pkg.requirements_md]
 
-	# Stage 1: Collect all requirements
+	# Stage 1: Collect all requirements / test traces
 	all_reqs = []
+	all_test_traces =[]
+
 	for pkg in packages:
 		for file in pkg.requirements_md:
 			valid_reqs = _parse_requirements(file)
 			all_reqs.extend(valid_reqs)
+
+		for file in pkg.tests:
+			traces = _collect_pytest(file)
+			all_test_traces.extend(traces)
+
+	print("Number of Requirements", len(all_reqs))
+	print("Number of tests", len(all_test_traces))
+	
