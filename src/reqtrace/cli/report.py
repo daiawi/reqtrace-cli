3aacc1adda84c83abe025facd7707d45c1beef91
daniel <daiawi972@gmail.com>
2026-08-17 22:56:28 -0400

    Move extract req trace to core

--- src/reqtrace/cli/report.py
+++ src/reqtrace/cli/report.py
@@ -3,9 +3,7 @@ from pathlib import Path
 import click
 
 from ..core.discover import find_packages
-from ..core.models import Package, RequirementTrace
-from ..core.parse import collect_pytest, parse_requirements_file
-from ..core.trace import build_requirement_test_map
+from ..core.trace import extract_req_trace
 
 
 @click.command()
@@ -24,29 +22,6 @@ def report(dir):
 
 	# Stage 1: Collect all requirements / test traces
 	for pkg in packages:
-		reqtraces = _extract_req_trace(pkg)
+		reqtraces = extract_req_trace(pkg)
 		print(reqtraces)
 
-
-def _extract_req_trace(package: Package) -> list[RequirementTrace]:
-	requirements = []
-	for file in package.requirements_md:
-		parsed_reqs = parse_requirements_file(file)
-		requirements.extend(parsed_reqs.valid_requirements)
-
-	pkg_test_traces = []
-	for file in package.tests:
-		traces = collect_pytest(file)
-		pkg_test_traces.extend(traces)
-
-	req_to_test = build_requirement_test_map(pkg_test_traces)
-
-	req_traces = []
-	for req in requirements:
-		req_traces.append(RequirementTrace(
-		req_id=req.id,
-		description=req.description,
-		test_ids=req_to_test.get(req.id, [])
-		))
-
-	return req_traces
--- src/reqtrace/core/trace.py
+++ src/reqtrace/core/trace.py
@@ -1,18 +1,8 @@
 from collections import defaultdict
 
-from .models import Requirement, RequirementTrace, TestTrace
+from .models import Package, RequirementTrace, TestTrace
+from .parse import collect_pytest, parse_requirements_file
 
-
-def trace_requirement(requirement: Requirement, tests: list[TestTrace]) -> RequirementTrace:
-	id = requirement.id
-	description = requirement.description
-	test_ids = [
-        test.test_id
-        for test in tests
-        if requirement.id in test.req_ids
-    ]
-
-	return RequirementTrace(req_id=id, description=description, test_ids=test_ids)
 
 def build_requirement_test_map(traces: list[TestTrace]):
 	requirements = defaultdict(list)
@@ -22,3 +12,26 @@ def build_requirement_test_map(traces: list[TestTrace]):
 			requirements[req_id].append(trace.test_id)
 
 	return requirements
+
+def extract_req_trace(package: Package) -> list[RequirementTrace]:
+	requirements = []
+	for file in package.requirements_md:
+		parsed_reqs = parse_requirements_file(file)
+		requirements.extend(parsed_reqs.valid_requirements)
+
+	pkg_test_traces = []
+	for file in package.tests:
+		traces = collect_pytest(file)
+		pkg_test_traces.extend(traces)
+
+	req_to_test = build_requirement_test_map(pkg_test_traces)
+
+	req_traces = []
+	for req in requirements:
+		req_traces.append(RequirementTrace(
+		req_id=req.id,
+		description=req.description,
+		test_ids=req_to_test.get(req.id, [])
+		))
+
+	return req_traces
